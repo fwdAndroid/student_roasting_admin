09fe8607a5f1b9344514a01f4c1fe65a8b50397e
fwdAndroid <fwdkaleem@gmail.com>
2023-04-15 06:38:35 +0500

    Result Done

--- lib/teacher_section/teacherclasees/student_result.dart
+++ lib/teacher_section/teacherclasees/student_result.dart
@@ -1,16 +1,157 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:student_roasting_admin/add_forms_admin/add_student.dart';
+import 'package:student_roasting_admin/teacher_section/teacherformsadd/add_teacher_result.dart';
+import 'package:student_roasting_admin/widgets/colors.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
+/// The home page of the application which hosts the datagrid.
 class StudentResult extends StatefulWidget {
-  const StudentResult({super.key});
+  /// Creates the home page.
+  StudentResult({Key? key}) : super(key: key);
 
   @override
-  State<StudentResult> createState() => _StudentResultState();
+  _StudentResultState createState() => _StudentResultState();
 }
 
 class _StudentResultState extends State<StudentResult> {
+  List<Employee> employees = <Employee>[];
+  late EmployeeDataSource employeeDataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    employees = getEmployeeData();
+    employeeDataSource = EmployeeDataSource(employeeData: employees);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: Container(
+        width: 200,
+        child: FloatingActionButton(
+          backgroundColor: AppColors.primary,
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (builder) => AddTeacherResult()));
+          },
+          child: Text("Create Result"),
+        ),
+      ),
+      body: SfDataGrid(
+        source: employeeDataSource,
+        selectionMode: SelectionMode.multiple,
+        columnWidthMode: ColumnWidthMode.fill,
+        columns: <GridColumn>[
+          GridColumn(
+              columnName: 'studentName',
+              label: Container(
+                  padding: EdgeInsets.all(16.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Student Name',
+                  ))),
+          GridColumn(
+              columnName: 'class',
+              label: Container(
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text('Class'))),
+          GridColumn(
+              columnName: 'designation',
+              label: Container(
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Subject',
+                    overflow: TextOverflow.ellipsis,
+                  ))),
+          GridColumn(
+              columnName: 'grade',
+              label: Container(
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Grade',
+                    overflow: TextOverflow.ellipsis,
+                  ))),
+        ],
+      ),
+    );
+  }
+
+  List<Employee> getEmployeeData() {
+    return [
+      Employee("James", 'Class A', 'Quran', 'D'),
+      Employee("Kathryn", 'Class B', 'Arabic', 'B'),
+      Employee("Lara", 'Class C', 'Quran', 'A+'),
+      Employee("Michael", 'Class D', 'Arabic', 'A'),
+      Employee("Martin", 'Class E', 'Quran', 'A'),
+      Employee("Newberry", 'Class F', 'Arabic', 'B'),
+      Employee("Balnc", 'Class G', 'Quran', 'A'),
+      Employee("Perry", 'Class H', 'Arabic', 'C'),
+      Employee("Gable", 'Class I', 'Quran', 'B'),
+      Employee("Grimes", 'Class J', 'Arabic', 'A')
+    ];
+  }
+}
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the employee which will be rendered in datagrid.
+class Employee {
+  /// Creates the employee class with required details.
+  Employee(
+    this.studentName,
+    this.name,
+    this.designation,
+    this.grade,
+  );
+
+  /// Id of an employee.
+  final String studentName;
+
+  /// Name of an employee.
+  final String name;
+
+  /// Designation of an employee.
+  final String designation;
+  final String grade;
+}
+
+/// An object to set the employee collection data source to the datagrid. This
+/// is used to map the employee data to the datagrid widget.
+class EmployeeDataSource extends DataGridSource {
+  /// Creates the employee data source class with required details.
+  EmployeeDataSource({required List<Employee> employeeData}) {
+    _employeeData = employeeData
+        .map<DataGridRow>((e) => DataGridRow(cells: [
+              DataGridCell<String>(
+                  columnName: 'studentName', value: e.studentName),
+              DataGridCell<String>(columnName: 'class', value: e.name),
+              DataGridCell<String>(
+                  columnName: 'designation', value: e.designation),
+              DataGridCell<String>(columnName: 'grade', value: e.grade),
+            ]))
+        .toList();
+  }
+
+  List<DataGridRow> _employeeData = [];
+
+  @override
+  List<DataGridRow> get rows => _employeeData;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(
+        cells: row.getCells().map<Widget>((e) {
+      return Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(8.0),
+        child: Text(e.value.toString()),
+      );
+    }).toList());
   }
 }
---
+++ lib/teacher_section/teacherformsadd/add_teacher_result.dart
@@ -0,0 +1,278 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:student_roasting_admin/teacher_section/teacher_widgets/teacher_siderbar.dart';
+import 'package:student_roasting_admin/widgets/colors.dart';
+import 'package:student_roasting_admin/widgets/exc_button.dart';
+import 'package:student_roasting_admin/widgets/input_text.dart';
+import 'package:student_roasting_admin/widgets/sidebar.dart';
+
+class AddTeacherResult extends StatefulWidget {
+  const AddTeacherResult({super.key});
+
+  @override
+  State<AddTeacherResult> createState() => _AddTeacherResultState();
+}
+
+class _AddTeacherResultState extends State<AddTeacherResult> {
+  TextEditingController studentname = TextEditingController();
+  TextEditingController studentgrade = TextEditingController();
+  TextEditingController studentcomments = TextEditingController();
+  TextEditingController studentsubject = TextEditingController();
+  TextEditingController studentclass = TextEditingController();
+
+  bool _isLoading = false;
+  String dropdownValue = 'Project Name';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primary,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text(
+          "Go Back",
+          textAlign: TextAlign.center,
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      ),
+      body: Row(
+        children: [
+          Container(
+            color: Color(0xff2294ed),
+            width: 260,
+            child: Column(
+              children: [
+                DrawerHeader(child: Image.asset("assets/logo.png")),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ListTile(
+                        selectedTileColor: Color(0xffeb4474),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (x) => TeacherSideDrawer()));
+                        },
+                        leading: Icon(Icons.person),
+                        iconColor: Colors.white,
+                        title: Text(
+                          'Student Portal',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      ListTile(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (x) => TeacherSideDrawer()));
+                        },
+                        leading: Icon(Icons.assignment),
+                        iconColor: Colors.white,
+                        title: Text(
+                          'Student Assignments',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      ListTile(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (x) => TeacherSideDrawer()));
+                        },
+                        leading: Icon(Icons.request_quote_outlined),
+                        iconColor: Colors.white,
+                        title: Text(
+                          'Students Result',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      ListTile(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (x) => TeacherSideDrawer()));
+                        },
+                        leading: Icon(Icons.notifications),
+                        iconColor: Colors.white,
+                        title: Text(
+                          'Notifications',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 320,
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              width: 448,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      "assets/logo.png",
+                      height: 220,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Student Name",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 9),
+                  InputText(
+                    controller: studentname,
+                    labelText: "Quran",
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {},
+                    onSaved: (val) {},
+                    textInputAction: TextInputAction.done,
+                    isPassword: false,
+                    enabled: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Student Class",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 9),
+                  InputText(
+                    controller: studentclass,
+                    labelText: "Class A",
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {},
+                    onSaved: (val) {},
+                    textInputAction: TextInputAction.done,
+                    isPassword: false,
+                    enabled: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Student Subject",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 9),
+                  InputText(
+                    controller: studentsubject,
+                    labelText: "Quran",
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {},
+                    onSaved: (val) {},
+                    textInputAction: TextInputAction.done,
+                    isPassword: false,
+                    enabled: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Student Grade",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 9),
+                  InputText(
+                    controller: studentgrade,
+                    labelText: "A",
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {},
+                    onSaved: (val) {},
+                    textInputAction: TextInputAction.done,
+                    isPassword: false,
+                    enabled: true,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Comments",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 9),
+                  InputText(
+                    controller: studentcomments,
+                    labelText: "He Need to work hard",
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {},
+                    onSaved: (val) {},
+                    textInputAction: TextInputAction.done,
+                    isPassword: false,
+                    enabled: true,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: WonsButton(
+                      height: 50,
+                      width: 348,
+                      verticalPadding: 0,
+                      color: AppColors.primary,
+                      child: const Text(
+                        "Send",
+                        style: TextStyle(
+                            color: AppColors.neutral,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
